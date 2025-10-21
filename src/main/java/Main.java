3673032b3aa1e58077588d78170b167bbfb974b4
maeven <72821815+notmaeven@users.noreply.github.com>
2025-10-21 19:32:10 +0200

    solved Main.java
    omg I can't believe I just solved this terribly hard coding task of printing "Hello MEITREX", I'm about to create the next billion dollar software tech startup with my superior coding skills.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        // TODO: Gib "Hello MEITREX" auf der Konsole aus
+        // TODO: Print "Hello MEITREX" to the console
+        System.out.println("Hello MEITREX");
+        // istg why am I even doing this and why is the task in German, comment your code in English for the love of all that is dairy. Yes. Milk joke. I miss Dani.
     }
 }
